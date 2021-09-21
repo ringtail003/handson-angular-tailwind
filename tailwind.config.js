5d9abcad63a39214765ac9068430f3d48aab111d
ringtail003 <ringtail003@gmail.com>
2021-09-21 21:50:12 +0900

    devモードでpurgeしないように

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: false,
+    content: ['./src/**/*.{html,ts}'],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
