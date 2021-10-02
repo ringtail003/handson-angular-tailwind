747e262ba18d87d3ff4be0839155566399d480ae
ringtail003 <ringtail003@gmail.com>
2021-10-02 12:01:23 +0900

    デモ用に試してみる

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { SpinnerComponent } from './spinner/spinner.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, SpinnerComponent],
+  imports: [BrowserModule, CommonModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,62 @@
+const theme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
+  mode: 'jit',
   purge: {
-    enabled: false,
-    content: ['./src/**/*.{html,ts}'],
+    enabled: process.env.TAILWIND_MODE === 'build',
+    content: [
+      './src/**/*.{html,ts}',
+      './src/**/*.js',
+    ],
   },
-  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        product: {
+          50: "#F3FAFC",
+          100: "#E9F4F7",
+          200: "#C6ECF7",
+          300: "#7AC7DE",
+          400: "#5CBEDC",
+          500: "#0096C4",
+          600: "#0083AB",
+          700: "#006F91",
+          800: "#005C78",
+          900: "#00485E",
+        },
+        foo: {
+          50: "#CCDAF0",
+          100: "#B3C7E8",
+          200: "#9AB4E1",
+          300: "#80A2D9",
+          400: "#678FD1",
+          500: "#4D7DCA",
+          600: "#346AC2",
+          700: "#2762C2",
+          800: "#1D5BC0",
+          900: "##0644AA",
+        },
+        bar: {
+          50: "#F2BFCA",
+          100: "#EC9FAF",
+          200: "#E68094",
+          300: "#DF6079",
+          400: "#D9405F",
+          500: "#D22044",
+          600: "#CC0029",
+          700: "#BA0429",
+          800: "#A60828",
+          900: "#8C0521",
+        },
+        callout: colors.indigo,
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+  ]
 }
